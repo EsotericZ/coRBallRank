ec6b378e048fd1b1cf834807ab360060d4a7b199
EsotericZ <cjsand03@gmail.com>
2022-04-02 09:17:14 -0600

    front end framework for login/add user

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,26 @@
+import { useQuery } from '@apollo/client';
+import { Routes, Route } from 'react-router-dom';
+import { Login } from './login/Login';
+import { Users } from './users/Users';
+
 function App() {
   return (
     <div>
+      <Routes>
+        <Route
+          path='/'
+          element={<Login/>}
+        />
+        <Route
+          path='/success'
+          element={<h1>WAGMI</h1>}
+        />
+        <Route
+          path='/users'
+          element={<Users/>}
+        />
+
+      </Routes>
     </div>
   );
 }
---
+++ client/src/users/Users.js
@@ -0,0 +1,18 @@
+import { useQuery } from '@apollo/client';
+import { FETCH_USERS } from '../graphql/queries/fetchUsers';
+
+export const Users = () => {
+	const {data, loading, error} = useQuery(FETCH_USERS);
+
+	console.log(error);
+	if (error) {
+		return <h1>Please login</h1>;
+	}
+
+	return loading ?
+		<h1>Loading....</h1>
+		:
+		<div>
+			Success
+		</div>
+}
