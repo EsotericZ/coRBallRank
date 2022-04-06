89302c37843bf47396a6b063ac045753c3451176
EsotericZ <cjsand03@gmail.com>
2022-04-06 16:01:00 -0600

    added more seeds

--- server/models/Match.js
+++ server/models/Match.js
@@ -3,9 +3,8 @@ const { Schema, model } = require('mongoose');
 const matchSchema = new Schema({
     matchId: { type: String, unique: true },
 	winningPlayerId: { type: Schema.Types.ObjectId, ref: 'Player' },
-    winningPointDifferential: Number,
     losingPlayerId: { type: Schema.Types.ObjectId, ref: 'Player' },
-    losingPointDifferential: Number,
+    pointDifferential: Number,
     score: String,
     tieBreaker: Boolean,
     division: String,
