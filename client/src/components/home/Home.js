050dce0b06894270512263e7a91041092ff03e13
Erna Lukac <e.lukac4@gmail.com>
2022-04-05 21:02:57 -0600

    skeleton homepage format

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,18 +1,37 @@
+import * as React from 'react';
 import HomeNav from '../homenav/HomeNav';
+import HomeMain from './HomeMain.js';
+import HomeEvents from './HomeEvents.js';
+import HomeRanking from './HomeRanking.js';
 import './home.css';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import { styled } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
 
 const Home = () => {
     return (
         <div>
             <HomeNav />
             <h1>TAKE ME HOME!! to the place!!! i BELONG!!!  </h1>
-            {/* <HomeMain />
-            <HomeRanking />
-            <HomeEvents /> */}
+            <Box sx={{ width: '100%' }}>
+                <Grid container direction="row-reverse" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={6}>
+                        <HomeRanking />
+
+                    </Grid>
+                    <Grid item xs={6}> 
+                        <HomeMain />
+                    </Grid>
+                    <Grid item xs={6}>
+                        <HomeEvents />
+                    </Grid>
+                </Grid>
+            </Box>
+
         </div>
     )
 };
-
 
 export default Home;
--- client/src/components/home/HomeMain.js
+++ client/src/components/home/HomeMain.js
@@ -1,1 +1,13 @@
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+
+const HomeMain = () => {
+    return (
+        <div>
+            <h1>Home Main </h1>
+
+        </div>
+    )
+};
+
+
+export default HomeMain;
