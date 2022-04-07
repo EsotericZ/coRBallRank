0f5495c6d1d3dc0908990787476c473ef02a4b25
Erna Lukac <e.lukac4@gmail.com>
2022-04-06 18:17:13 -0600

    mobile optimization for home page

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import HomeNav from '../homenav/HomeNav';
 import HomeMain from './HomeMain.js';
 import HomeEvents from './HomeEvents.js';
-import HomeRanking from './HomeRanking.js';
+import Ranking from '../ranking/Ranking.js';
 import './home.css';
 import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
 import { styled } from '@mui/material/styles';
@@ -16,15 +16,17 @@ const Home = () => {
             <HomeNav />
             <h1>TAKE ME HOME!! to the place!!! i BELONG!!!  </h1>
             <Box sx={{ width: '100%' }}>
-                <Grid container direction="row-reverse" rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
-                    <Grid item xs={6}>
-                        <HomeRanking />
-
-                    </Grid>
-                    <Grid item xs={6}> 
+                <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+                    <Grid item xs={12} md={6}>
                         <HomeMain />
                     </Grid>
-                    <Grid item xs={6}>
+
+                    <Grid item xs={12} md={6}>
+                        <Ranking />
+
+                    </Grid>
+
+                    <Grid item xs={12} md={6} id="homeEvents">
                         <HomeEvents />
                     </Grid>
                 </Grid>
