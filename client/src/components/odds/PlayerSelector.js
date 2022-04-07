fd445b7ee2e09978614a10feda299958695ddc95
EsotericZ <cjsand03@gmail.com>
2022-04-06 19:25:00 -0600

    worked on odds page

---
+++ client/src/components/odds/PlayerSelector.js
@@ -0,0 +1,14 @@
+const PlayerSelector = () => {
+    // const [Player1, setPlayer1] = useState('');
+    // const [Player2, setPlayer2] = useState('');
+    // const handleChange = (event) => {
+    //     setPlayer1(event.target.value);
+    //     setPlayer2(event.target.value);
+    // };
+
+    return (
+        <h1>!</h1>
+    )
+};
+
+export default PlayerSelector;
