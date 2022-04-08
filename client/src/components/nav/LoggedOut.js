56dc2c042f96c81671fc9184f9b50fe77314e6a0
kirtleyadams <hello@kirtleymichelle.com>
2022-04-08 13:26:48 -0400

    nav color

--- client/src/components/nav/LoggedOut.js
+++ client/src/components/nav/LoggedOut.js
@@ -13,6 +13,7 @@ import DrawerComponent from "./LoggedOutDrawer";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import './loggedOut.css'
+import '../../index.css';
 import { spacing } from "@mui/system";
 import  rockies from "../../assets/cologo.png";
 
@@ -20,7 +21,7 @@ import Login from "../login/Login";
 
 const useStyles = makeStyles((theme) => ({
   navlinks: {
-    marginLeft: theme.spacing(),
+    marginLeft: theme.spacing(5),
     marginTop: theme.spacing(),
     marginRight: theme.spacing(),
     // display: "inline-block",
@@ -30,24 +31,21 @@ const useStyles = makeStyles((theme) => ({
     flexGrow: "1",
     cursor: "pointer",
   },
-  link: {
-    textDecoration: "none",
-    color: "white",
-    fontSize: "18px",
-    marginLeft: theme.spacing(),
-    "&:hover": {
-      color: "red",
-      borderBottom: "1px solid white",
-    },
-    marginRight: theme.spacing(),
-    "&:hover": {
-      color: "red",
-      borderBottom: "1px solid white",
-    },
-    logOutNav: {
-      backgroundColor:"green",
-     }
-  },
+  // link: {
+  //   textDecoration: "none",
+  //   color: "white",
+  //   fontSize: "18px",
+  //   marginLeft: theme.spacing(),
+  //   "&:hover": {
+  //     color: "red",
+  //     borderBottom: "1px solid white",
+  //   },
+  //   marginRight: theme.spacing(10),
+  //   "&:hover": {
+  //     color: "red",
+  //     borderBottom: "1px solid white",
+  //   },
+  // },
 }));
 
 function LoggedOut() {
@@ -56,29 +54,29 @@ function LoggedOut() {
   const isMobile = useMediaQuery(("md"));
 
   return (
-    <AppBar position="static"className="logoutNav"theme={theme}>
-      <CssBaseline className="logoutNav" theme={theme}/>
-      <Toolbar className="logoutNav"theme={theme}>
+    <AppBar position="static"className="logoutNav">
+      <CssBaseline className="logoutNav" />
+      <Toolbar className="logoutNav">
         <Typography variant="h4" className={classes.logo}>
           <img src={rockies} alt="logo" className="logo"/>
         </Typography>
         {isMobile ? (
           <DrawerComponent />
         ) : (
-          <div className={classes.navlinks} id="logOutNav"theme={theme}>
-            <Link to="/home" className={classes.link}>
+          <div className={classes.navlinks} id="logOutNav">
+            <Link to="/home" className={classes.link} id="home">
               Home
             </Link>
-            <Link to="/ranking" className={classes.link}>
+            <Link to="/ranking" className={classes.link} id="ranking">
               Ranking
             </Link>
-            <Link to="/events" className={classes.link}>
+            <Link to="/events" className={classes.link}id="events">
               Events
             </Link>
-            <Link to="/history" className={classes.link}>
+            <Link to="/history" className={classes.link}id="history">
               History
             </Link>
-            <Link to="/odds" className={classes.link}>
+            <Link to="/odds" className={classes.link}id="odds">
               Odds
             </Link>
             <Link to="/create" className={classes.link}>
