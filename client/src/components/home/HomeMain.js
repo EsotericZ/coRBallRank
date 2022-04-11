eb7ce9db7f3f7c2095a2aa51c096d71fc118e979
EsotericZ <cjsand03@gmail.com>
2022-04-10 19:06:21 -0600

    css for the rankings page

--- client/src/components/home/HomeMain.js
+++ client/src/components/home/HomeMain.js
@@ -1,10 +1,11 @@
-import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+// import { DataGrid, GridRowsProp, GridColDef } from '@mui/x-data-grid';
+import PDF from '../../../src/assets/2020SINGLES.png';
 
 const HomeMain = () => {
     return (
         <div>
             <h1>Home Main </h1>
-
+            <img src={PDF} alt="Singles" />
         </div>
     )
 };
