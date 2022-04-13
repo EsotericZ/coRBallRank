4eeedf942d9947e0e551a73739c19e75f288c3b7
kai <kai@kaiconsulting.co>
2022-04-12 18:45:20 -0600

    styling

--- client/src/components/home/Complete.js
+++ client/src/components/home/Complete.js
@@ -7,13 +7,15 @@ const Complete = ({ events }) => {
                 {events.map((event) => (
                     <>
                         <img className="completedImg" src={event.image} alt="Event Pic" height="200" />
-                        <div className="completedName">{event.name}</div>
-                        <div className="completedLocation">{event.club}, {event.location}</div>
-                        <div className="completedDate">{event.startDate} - {event.endDate}</div>
-                        Event Information 
-                        <a href={event.link} target="_blank" className="completedLink">
-                            Here
-                        </a>
+                        <div className="eventText">
+                            <div className="completedName">{event.name}</div>
+                            <div className="completedLocation">{event.club}, {event.location}</div>
+                            <div className="completedDate">{event.startDate} - {event.endDate}</div>
+                            Event Information
+                            <a href={event.link} target="_blank" className="completedLink">
+                                Here
+                            </a>
+                        </div>
                     </>
                 ))}
             </div>
